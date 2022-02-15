7fb1b5fc83ff3cf1b43caac975ff00dac193a96c
Arthur <arthurnunes.petee@gmail.com>
2022-02-14 22:17:33 -0300

    Update animation saving method, add graphs plot

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
+import time
 import shutil
-from math import cos, sin
+import cv2
+from math import cos, sin, pi
 import numpy as np
 import matplotlib.animation as ani
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
-from PIL import Image
+# from mpl_toolkits.mplot3d import axes3d
+# from PIL import Image
 import logging
 import logging.handlers
 from tqdm import tqdm
@@ -31,7 +33,7 @@ logger.addHandler(file_handler)
 class Simulator():
 
     _TMP_ANIMATION_DIR = 'animation'
-    _ANIMATION_OUTPUT_FILE = 'animation.gif'
+    _ANIMATION_OUTPUT_FILE = 'animation.mp4'
     
     def __init__(self,groups,params,initial_poses):
         number = 0
@@ -51,17 +53,20 @@ class Simulator():
     def run(self, dt = 0.1, t_start = 0.0, t_stop = 100.0):
         logger.info('Running simulator')
         self._time_array = np.linspace(t_start, t_stop, int((t_stop - t_start) / dt + 1))
+        self._segragation_indexes = []
+        self._random_colors = np.random.randint(0, 255, [self._n_groups+1, 3])
         # with alive_bar(1000) as bar:
-        #     for i in self.run_loop(dt=dt):
+        #     for i in self._run_loop(dt=dt):
         #         bar()
-        for i in tqdm(self.run_loop(dt=dt)):
+        for i in tqdm(self._run_loop(dt=dt)):
             pass
         logger.info('Simulation finished')
 
-    def run_loop(self, dt = 0.1):
+    def _run_loop(self, dt = 0.1):
         for t in self._time_array:
             # All send/recieve memory info
-            self.communicate()
+            self._communicate_robots()
+            self._segragation_indexes.append(self._compute_segregation_index())
             # For each robot
             for i in range(len(self._dubins_control)):
                 robot = self._dubins_control[i]
@@ -87,7 +92,7 @@ class Simulator():
                 robot.set_pose2D(robot_pose)
             yield
 
-    def communicate(self):
+    def _communicate_robots(self):
         for i in range(self._n_robots):
             for j in range(i+1,self._n_robots):
                 robot_i = self._dubins_control[i]
@@ -100,46 +105,96 @@ class Simulator():
                     robot_i.recieve_memory(j_memory)
                     robot_j.recieve_memory(i_memory)
 
-    def animate(self, skip = 10, fixed_axes = True):
+    def _compute_segregation_index(self):
+        home_zones = {}
+        for robot_i in self._dubins_control:
+            i_group = robot_i.get_group()
+            i_position = np.array( robot_i.get_pose2D()[:2] )
+            i_curve = int(round(np.linalg.norm(i_position)/self._params['d']))
+            if i_group in home_zones:
+                if i_curve < home_zones[i_group]['inner']:
+                    home_zones[i_group]['inner'] = i_curve
+                if i_curve > home_zones[i_group]['outer']:
+                    home_zones[i_group]['outer'] = i_curve
+            else:
+                home_zones[i_group] = {'inner': i_curve, 'outer': i_curve}
+        index = 0.0
+        for robot_i in self._dubins_control:
+            i_group = robot_i.get_group()
+            i_position = np.array( robot_i.get_pose2D()[:2] )
+            i_curve = int(round(np.linalg.norm(i_position)/self._params['d']))
+            for group, zone in home_zones.items():
+                if group == i_group:
+                    continue
+                if i_curve >= zone['inner'] and i_curve <= zone['outer']:
+                    index = index + 1.0
+        return 1 - (index/(self._n_robots*self._n_groups))
+
+    def animate(self, interval = 50, fps = 30):
         logger.info('Creating animation')
-        # Create animation tmp dir
         if os.path.isfile(self._ANIMATION_OUTPUT_FILE):
             os.remove(self._ANIMATION_OUTPUT_FILE)
-        if not os.path.isdir(self._TMP_ANIMATION_DIR):
-            os.mkdir(self._TMP_ANIMATION_DIR)
         # Animation loop
-        frame_rate = range(0,len(self._time_array),skip)
-        random_colors = np.random.randint(0, 255, [self._n_groups+1, 3])
-        # with alive_bar(1000) as bar:
-        #     for i in self.animate_loop(frame_rate,random_colors,fixed_axes=fixed_axes):
-        #         bar()
-        for i in tqdm(self.animate_loop(frame_rate,random_colors,fixed_axes=fixed_axes)):
-            pass
-        # Export result
-        images = [Image.open(f"{self._TMP_ANIMATION_DIR}/{n}.jpg") for n in frame_rate]
-        images[0].save(self._ANIMATION_OUTPUT_FILE, save_all=True, append_images=images[1:], duration=skip, loop=0)
-        # Cleanup animation tmp
-        shutil.rmtree(self._TMP_ANIMATION_DIR)
-        logger.info('Animation finished')
+        fig = plt.figure(figsize=(20, 20))
+        ax = plt.axes(xlim=(-40, 40), ylim=(-40, 40))
+        self._robots_plot = []
+        n = 0
+        for robot_id in range(len(self._states_array)):
+            pose = self._states_array[robot_id][n]
+            x = pose[0]; y = pose[1]; theta = pose[2]
+            traj_x = [pose[0] for pose in self._states_array[robot_id][0:n]]; traj_y = [pose[1] for pose in self._states_array[robot_id][0:n]]; traj_theta = [pose[2] for pose in self._states_array[robot_id][0:n]]
+            group = self._dubins_control[robot_id].get_group()
+            color = self._random_colors[group,:]/255
+            pose_plot, = ax.plot(x,y,'o',markersize=self._params['Rb'], color = color)
+            traj_plot, = ax.plot(traj_x, traj_y, color = color,linewidth=3)
+            self._robots_plot.append({
+                'position': pose_plot,
+                'trajectory': traj_plot
+            })
+        plt.xlabel('x'); plt.ylabel('y')
+        plt.gca().set_aspect('equal', adjustable='box')
+        plt.grid(True)
+        anim = ani.FuncAnimation(fig, self._animate_loop, frames = len(self._time_array), interval = interval, blit = False)
+        plt.tight_layout()
+        # plt.show()
+        logger.info('Exporting animation file')
+        anim.save(self._ANIMATION_OUTPUT_FILE, fps = fps, writer = 'ffmpeg')
+        logger.info('Exported animation file to ' + str(self._ANIMATION_OUTPUT_FILE))
+     
+    def _animate_loop(self,n):
+        for robot_id in range(len(self._states_array)):
+            pose = self._states_array[robot_id][n]
+            x = pose[0]; y = pose[1]; theta = pose[2]
+            traj_x = [pose[0] for pose in self._states_array[robot_id][0:n]]; traj_y = [pose[1] for pose in self._states_array[robot_id][0:n]]; traj_theta = [pose[2] for pose in self._states_array[robot_id][0:n]]
+            self._robots_plot[robot_id]['position'].set_data(x,y)
+            self._robots_plot[robot_id]['trajectory'].set_data(traj_x,traj_y)
+        
+    def plot_results(self):
+        logger.info('Plotting results')
+        fig = plt.figure()
+        plt.plot(self._time_array, self._segragation_indexes)
+        plt.grid(True)
+        plt.xlabel('t'); plt.ylabel('Segregation Index')
+        plt.title('Segregation Index')
+        # plt.show(block=False)
+        plt.savefig('segregation_index.jpg')
+        logger.info('Saved file segregation_index.jpg')
 
-    def animate_loop(self, frame_rate, random_colors, fixed_axes = True):
-        for n in frame_rate:
-            fig = plt.figure(figsize=(20, 20))
-            for robot_id in range(len(self._states_array)):
-                pose = self._states_array[robot_id][n]
-                x = pose[0]; y = pose[1]; theta = pose[2]
-                traj_x = [pose[0] for pose in self._states_array[robot_id][0:n]]; traj_y = [pose[1] for pose in self._states_array[robot_id][0:n]]; traj_theta = [pose[2] for pose in self._states_array[robot_id][0:n]]
-                group = self._dubins_control[robot_id].get_group()
-                color = random_colors[group,:]/255
-                plt.scatter(x,y,s=100,marker='o', color = color)
-                plt.plot(traj_x, traj_y, color = color,linewidth=3)
-                plt.grid(True)
-                if fixed_axes:
-                    plt.xlim([40, -40]);  plt.ylim([40, -40])
-            plt.xlabel('x'); plt.ylabel('y')
-            plt.savefig(f"{self._TMP_ANIMATION_DIR}/{n}.jpg")
-            plt.close()
-            yield
+        fig = plt.figure()
+        for robot_id in range(len(self._states_array)):
+            pose = self._states_array[robot_id][-1]
+            x = pose[0]; y = pose[1]; theta = pose[2]
+            traj_x = [pose[0] for pose in self._states_array[robot_id][:]]; traj_y = [pose[1] for pose in self._states_array[robot_id][:]]; traj_theta = [pose[2] for pose in self._states_array[robot_id][:]]
+            group = self._dubins_control[robot_id].get_group()
+            color = self._random_colors[group,:]/255
+            plt.scatter(x,y,s=pi*self._params['Rb']**2,marker='o', color = color)
+            plt.plot(traj_x, traj_y, color = color,linewidth=3)
+            plt.grid(True)
+            plt.gca().set_aspect('equal', adjustable='box')
+        # plt.show(block=False)
+        # plt.show()
+        plt.savefig('trajectory.jpg')
+        logger.info('Saved file trajectory.jpg')
 
 
 if __name__ == '__main__':
@@ -149,13 +204,15 @@ if __name__ == '__main__':
         'c': 36.0,
         'ref_vel': 1.0
     }
-    groups = [0,0,2,1]
+    groups = [0,0,0,1,1]
     initial_poses = [
-        [10.0, 0.0, 0.0],
-        [0.0, 20.0, 0.0],
-        [0.0, 30.0, 0.0],
-        [20.0, 0.0, 0,0]
+        [10.0, 0.0, -pi/2],
+        [0.0, 20.0, -pi],
+        [-20.0, 0.0, -pi/2],
+        [20.0, 0.0, pi/2],
+        [40.0, 0.0, pi/2]
     ]
     sim = Simulator(groups, params, initial_poses)
-    sim.run(dt = 0.5, t_start = 0.0, t_stop = 6000.0)
-    sim.animate(skip = 20)
+    sim.run(dt = 0.1, t_start = 0.0, t_stop = 300.0)
+    sim.plot_results()
+    sim.animate()
